4cdd03d9d78864fae4182ad6641245027d676688
Kasthuri <kasthuri93441@gmail.com>
2024-08-28 08:40:53 +0530

    Added

---
+++ Java_Programs_Practice/src/DynamicProgramming/stocks/BuyAndSellStocksWithCooldown.java
@@ -0,0 +1,127 @@
+package DynamicProgramming.stocks;
+
+import java.util.Arrays;
+
+public class BuyAndSellStocksWithCooldown {
+
+	public static void main(String[] args) {
+		
+		int n=5,prices[]= {1,2,3,0,2};
+		
+//		System.out.println(buyAndSellStocksWithCooldownRecursion(0,0,n,prices));
+//		System.out.println(buyAndSellStocksWithCooldownMemoization(n, prices));
+//		System.out.println(buyAndSellStocksWithCooldownTabulation(n, prices));
+		System.out.println(buyAndSellStocksWithCooldownSpaceOptimized(n, prices));
+
+	}
+
+	private static int buyAndSellStocksWithCooldownSpaceOptimized(int n, int[] prices) {
+		
+		int front1[]=new int[2];
+		int front2[]=new int[2];
+		int curr[]=new int[2];
+		
+		for(int index=n-1;index>=0;index--)
+		{
+			for(int buy=0;buy<=1;buy++)
+			{
+				int profit=0;
+				
+				if(buy==0)
+				{
+					profit=Math.max(0+front1[0],-prices[index]+front1[1]);
+				}
+				if(buy==1)
+				{
+					profit=Math.max(0+front1[1],prices[index]+front2[0]);
+				}
+				
+				curr[buy]=profit;
+			}
+			
+			front2=front1.clone();
+			front1=curr.clone();
+		}
+		return front1[0];
+	}
+
+	private static int buyAndSellStocksWithCooldownTabulation(int n, int[] prices) {
+		
+		int dp[][]=new int[n+2][2];
+		
+		for(int index=n-1;index>=0;index--)
+		{
+			for(int buy=0;buy<=1;buy++)
+			{
+				int profit=Integer.MIN_VALUE;
+				
+				if(buy==0)
+				{
+					profit=Math.max(0+dp[index+1][0],-prices[index]+dp[index+1][1]);
+				}
+				
+				if(buy==1)
+				{
+					profit=Math.max(0+dp[index+1][1],prices[index]+dp[index+2][0]);
+				}
+				
+				dp[index][buy]=profit;
+			}
+		}
+		return dp[0][0];
+	}
+
+	private static int buyAndSellStocksWithCooldownMemoization(int n, int[] prices) {
+		
+		int dp[][]=new int[n][2];
+		
+		for(int row[]:dp)
+		{
+			Arrays.fill(row,-1);
+		}
+		return buyAndSellStocksWithCooldownMemo(0, 0,n,prices,dp);
+	}
+
+	private static int buyAndSellStocksWithCooldownMemo(int index, int buy, int n, int[] prices,int dp[][]) {
+		
+		if(index>=n) return 0;
+		
+		if(dp[index][buy]!=-1) return dp[index][buy];
+		
+		int profit=Integer.MIN_VALUE;
+		
+		if(buy==0)
+		{
+			profit=Math.max(0+buyAndSellStocksWithCooldownMemo(index+1,0, n, prices, dp),
+					-prices[index]+buyAndSellStocksWithCooldownMemo(index+1,1, n, prices, dp));
+		}
+		
+		if(buy==1)
+		{
+			profit=Math.max(0+buyAndSellStocksWithCooldownMemo(index+1,1, n, prices, dp),
+					prices[index]+buyAndSellStocksWithCooldownMemo(index+2,0, n, prices, dp));
+		}
+		
+		return dp[index][buy]=profit;
+	}
+
+	private static int buyAndSellStocksWithCooldownRecursion(int index, int buy, int n, int[] prices) {
+
+		if(index>=n) return 0;
+		
+		int profit=Integer.MIN_VALUE;
+		
+		if(buy==0)
+		{
+			profit=Math.max(0+buyAndSellStocksWithCooldownRecursion(index+1,0, n, prices),
+					-prices[index]+buyAndSellStocksWithCooldownRecursion(index+1,1, n, prices));
+		}
+		
+		if(buy==1)
+		{
+			profit=Math.max(0+buyAndSellStocksWithCooldownRecursion(index+1, 1, n, prices),
+					prices[index]+buyAndSellStocksWithCooldownRecursion(index+2,0, n, prices));
+		}
+		return profit;
+	}
+}
---
+++ Java_Programs_Practice/src/DynamicProgramming/stocks/BuyAndSellStocksWithTransactionFee.java
@@ -0,0 +1,143 @@
+package DynamicProgramming.stocks;
+
+import java.util.Arrays;
+
+public class BuyAndSellStocksWithTransactionFee {
+
+	public static void main(String[] args) {
+		
+		int n = 6,prices[] = {1,3,2,8,4,9},fee = 2;
+		
+//		System.out.println(buyAndSellStocksWithTransactionFeeRecursion(0,0,n,prices,fee));
+//		System.out.println(buyAndSellStocksWithTransactionFeeMemoization(0,0, n, prices, fee));
+//		System.out.println(buyAndSellStocksWithTransactionFeeTabulation(n, prices, fee));
+//		System.out.println(buyAndSellStocksWithTransactionFeeSpaceOptimized1(n, prices, fee));
+		System.out.println(buyAndSellStocksWithTransactionFeeSpaceOptimized2(n, prices, fee));
+		
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeSpaceOptimized2(int n, int[] prices, int fee) {
+		
+		int currNotBuy,frontNotBuy=0,frontBuy=0,currBuy;
+
+		for(int index=n-1;index>=0;index--)
+		{
+			currNotBuy=Math.max(0+frontNotBuy,-prices[index]+frontBuy);
+			currBuy=Math.max(0+frontBuy,prices[index]-fee+frontNotBuy);
+			
+			frontNotBuy=currNotBuy;
+			frontBuy=currBuy;
+		}
+		
+		return frontNotBuy;
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeSpaceOptimized1(int n, int[] prices, int fee) {
+
+		int front[]=new int[2];
+		int curr[]=new int[2];
+		
+		for(int index=n-1;index>=0;index--)
+		{
+			for(int buy=0;buy<=1;buy++)
+			{
+				int profit=Integer.MIN_VALUE;
+				
+				if(buy==0)
+				{
+					profit=Math.max(0+front[0],
+							-prices[index]+front[1]);
+				}
+				
+				if(buy==1)
+				{
+					profit=Math.max(0+front[1],
+							prices[index]-fee+front[0]);
+				}
+				
+				curr[buy]=profit;
+			}
+			front=curr.clone();
+		}
+		return front[0];
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeTabulation(int n, int[] prices, int fee) {
+		
+		int dp[][]=new int[n+1][2];
+		
+		for(int index=n-1;index>=0;index--)
+		{
+			for(int buy=0;buy<=1;buy++)
+			{
+				int profit=Integer.MIN_VALUE;
+				
+				if(buy==0)
+				{
+					profit=Math.max(0+dp[index+1][0],-prices[index]+dp[index+1][1]);
+				}
+				
+				if(buy==1)
+				{
+					profit=Math.max(0+dp[index+1][1],prices[index]+dp[index+1][0]-fee);
+				}
+				
+				dp[index][buy]=profit;
+			}
+		}
+		return dp[0][0];
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeMemoization(int index, int buy, int n, int[] prices, int fee) {
+
+		int dp[][]=new int[n][2];
+		
+		for(int row[]:dp)
+		{
+			Arrays.fill(row,-1);
+		}
+		
+		return buyAndSellStocksWithTransactionFeeMemo(index, buy, n, prices, fee,dp);
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeMemo(int index, int buy, int n, int[] prices, int fee,int dp[][]) {
+		
+		if(index==n) return 0;
+		if(dp[index][buy]!=-1) return dp[index][buy];
+		
+		int profit=Integer.MIN_VALUE;
+		
+		if(buy==0)
+		{
+			profit=Math.max(0+buyAndSellStocksWithTransactionFeeMemoization(index+1,0, n, prices, fee),
+					-prices[index]+buyAndSellStocksWithTransactionFeeMemoization(index+1,1, n, prices, fee));
+		}
+		
+		if(buy==1)
+		{
+			profit=Math.max(0+buyAndSellStocksWithTransactionFeeMemoization(index+1,1, n, prices, fee),
+					prices[index]-fee+buyAndSellStocksWithTransactionFeeMemoization(index+1,0, n, prices, fee));
+		}
+		return dp[index][buy]=profit;
+	}
+
+	private static int buyAndSellStocksWithTransactionFeeRecursion(int index, int buy, int n, int[] prices,int fee) {
+		
+		if(index==n) return 0;
+		
+		int profit=Integer.MIN_VALUE;
+		
+		if(buy==0)
+		{
+			profit=Math.max(0+buyAndSellStocksWithTransactionFeeRecursion(index+1,0, n, prices,fee),
+					-prices[index]+buyAndSellStocksWithTransactionFeeRecursion(index+1,1, n, prices,fee));
+		}
+		
+		if(buy==1)
+		{
+			profit=Math.max(0+buyAndSellStocksWithTransactionFeeRecursion(index+1,1, n, prices,fee),
+					prices[index]-fee+buyAndSellStocksWithTransactionFeeRecursion(index+1,0, n, prices,fee));
+		}
+		return profit;
+	}
+}
