ef413517fe9e02f7418c266a6845e5ecfa5648f4
Kasthuri <kasthuri93441@gmail.com>
2024-12-19 09:19:33 +0530

    Added

--- Java_Programs_Practice/src/BinarySearchTree/medium/AggressiveCows.java
+++ Java_Programs_Practice/src/BinarySearchTree/medium/AggressiveCows.java
@@ -7,20 +7,26 @@ public class AggressiveCows {
 	public static void main(String[] args) {
 
 		int stalls[] = {0, 3, 4, 7, 10, 9};
-        int k = 4;
-        
-        System.out.println(aggressiveCowsBrute(stalls,k));
-        System.out.println(aggressiveCowsOptimal(stalls,k));
-		
+		int k = 4;
+
+		System.out.println(aggressiveCowsBrute(stalls,k));
+		System.out.println(aggressiveCowsOptimal(stalls,k));
+
 	}
+
+	/*
+	 * Time Complexity: O(NlogN) + O(N * log(max(stalls[])-min(stalls[])))
+	 * Space Complexity: O(1)
+	 * 
+	 */
 
 	private static int aggressiveCowsOptimal(int[] stalls, int k) {
 
 		int n=stalls.length;
 		Arrays.sort(stalls);
-		
+
 		int low=1,high=stalls[n-1]-stalls[0];
-		
+
 		while(low<=high)
 		{
 			int mid=(low+high)/2;
@@ -37,13 +43,19 @@ public class AggressiveCows {
 		return high;
 	}
 
+	/*
+	 * Time Complexity: O(NlogN) + O(N *(max(stalls[])-min(stalls[])))
+	 * Space Complexity: O(1)
+	 * 
+	 */
+
 	private static int aggressiveCowsBrute(int[] stalls, int k) {
 
 		int n=stalls.length;
 		Arrays.sort(stalls);
-		
+
 		int limit=stalls[n-1]-stalls[0];
-		
+
 		for(int i=1;i<=limit;i++)
 		{
 			if(!canWePlace(stalls,n,i,k))
@@ -55,10 +67,10 @@ public class AggressiveCows {
 	}
 
 	private static boolean canWePlace(int[] stalls,int n,int dist, int k) {
-		
+
 		int last=stalls[0];
 		int countCows=1;
-		
+
 		for(int i=1;i<n;i++)
 		{
 			if(stalls[i]-last>=dist)
@@ -66,7 +78,7 @@ public class AggressiveCows {
 				countCows++;
 				last=stalls[i];
 			}
-			
+
 			if(countCows>=k)
 				return true;
 		}
