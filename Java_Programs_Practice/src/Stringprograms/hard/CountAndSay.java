96ef8e0f71b35dd68c07c3d7d980188bb4d89406
Kasthuri <kasthuri93441@gmail.com>
2025-01-11 12:18:27 +0530

    Added

---
+++ Java_Programs_Practice/src/Stringprograms/hard/CountAndSay.java
@@ -0,0 +1,53 @@
+package Stringprograms.hard;
+
+public class CountAndSay {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		System.out.println(countAndSayOptimal(n));
+
+	}
+	/*
+	 * Time Complexity:O(n*m)
+	 * Space Complexity: O(m)
+	 */
+	private static String countAndSayOptimal(int n) {
+
+		String result="1";
+
+		if(n==1) return result;
+
+		for(int i=2;i<=n;i++)
+		{
+			result=buildNext(result);
+		}
+
+		return result;		
+	}
+
+	private static String buildNext(String result) {
+
+		StringBuilder sb=new StringBuilder();
+
+		int m=result.length(),count=1;
+
+		for(int i=0;i<m-1;i++)
+		{
+			if(result.charAt(i)==result.charAt(i+1))
+			{
+				count++;
+			}
+			else
+			{
+				sb.append(count).append(result.charAt(i));
+				count=1;
+			}
+		}
+
+		sb.append(count).append(result.charAt(m-1));
+
+		return sb.toString();
+	}
+}
