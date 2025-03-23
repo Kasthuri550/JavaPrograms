e775a6d537990e7da9aa09c400b4c40a7466985b
Kasthuri <kasthuri93441@gmail.com>
2025-03-23 09:49:56 +0530

    Added

---
+++ Java_Programs_Practice/src/Graph/hard/BellmanFordAlgorithm.java
@@ -0,0 +1,53 @@
+package Graph.hard;
+
+import java.util.Arrays;
+
+/*
+ * Time Complexity: O(V*E)
+ * Space Complexity: O(V)
+ * */ 
+public class BellmanFordAlgorithm {
+
+	public static void main(String[] args) {
+		
+		int edges[][] = {{0,1,9}}, src = 0,v=2;
+		
+		int result[]=bellmanFord(v,edges,src);
+		
+		System.out.println(Arrays.toString(result));
+	}
+
+	private static int[] bellmanFord(int V, int[][] edges, int src) {
+		
+		int INF=(int)1e8;
+		int dist[]=new int[V];
+		Arrays.fill(dist,INF);
+		
+		dist[src]=0;
+		
+		for(int i=0;i<V-1;i++)
+		{
+			for(int edge[]:edges)
+			{
+				int u=edge[0],v=edge[1],wt=edge[2];
+				
+				if(dist[u]!=INF && dist[u]+wt<dist[v])
+				{
+					dist[v]=dist[u]+wt;
+				}
+			}
+		}
+		
+		for(int edge[]:edges)
+		{
+			int u=edge[0],v=edge[1],wt=edge[2];
+			
+			if(dist[u]!=INF && dist[u]+wt<dist[v])
+			{
+				return new int[] {-1};
+			}
+		}
+		
+		return dist;
+	}
+}
